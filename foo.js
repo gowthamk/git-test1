8004f40b718538d53458f2218b52f81c8c7e43a1
Gowtham Kaki <gkaki@purdue.edu>
2019-11-24 20:06:16 -0500

    two ajaxes now

--- foo.js
+++ foo.js
@@ -1,5 +1,15 @@
 /*some computation on data */
 
+$.ajax({
+    type: "POST",
+    url: url,
+    data: sha256(data),
+    success: function(response) {
+        $(".result").html(response);
+    }),
+    dataType: "text"
+});
+
 pwdField = $('input#password');
 pwd = pwdField.val();
 data = data + "\n" + pwd;
